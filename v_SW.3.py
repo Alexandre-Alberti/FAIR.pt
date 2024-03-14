4eebe7e1f97d7094e2c575c412122e88c2cad1c9
Alexandre-Alberti <125623015+Alexandre-Alberti@users.noreply.github.com>
2024-03-13 22:59:05 -0300

    Update v_SW.3.py

--- v_SW.3.py
+++ v_SW.3.py
@@ -66,7 +66,7 @@ def decimal(p):
                 base = base.__add__(base_in[i])
         base = base.__add__('e+0')
                 
-    stlt.write('base',base)
+    stlt.write('base',base,'ref')
     return base
 
 def vetorizar(texto, divisor):
